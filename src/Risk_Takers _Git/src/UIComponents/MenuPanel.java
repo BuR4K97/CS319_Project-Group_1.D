fced0a6cee822ee460af9c27211fd2427fd886a4
burak33yeni <burak33yeni@gmail.com>
2019-05-08 00:57:09 +0300

    user friendly wave

--- src/Risk_Takers _Git/src/UIComponents/MenuPanel.java
+++ src/Risk_Takers _Git/src/UIComponents/MenuPanel.java
@@ -2,6 +2,11 @@ package UIComponents;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.io.*;
 import java.util.ArrayList;
 
@@ -15,6 +20,7 @@ public class MenuPanel extends JPanel {
 	private Pixel mapAnimation;
 	private ArrayList<VisualString> stringList;
 	private Fireworks fireWorks;
+	int mouseX = 0, mouseY = 0;
 
 	public MenuPanel() {
 		this.setPreferredSize(new Dimension(1920, 1080));
@@ -51,6 +57,39 @@ public class MenuPanel extends JPanel {
 		setBackground(Color.BLACK);
 
 		mapAnimation = new Pixel();
+
+		// timer for draw sea using mouse
+		Timer seaMouse = new Timer(50, new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mapAnimation.listCircle.add(new Circle( mouseX, mouseY));
+
+			}
+		});
+
+		MouseListener ml = new MouseListener() {
+			public void mouseReleased(MouseEvent e) {
+				seaMouse.stop();
+			}
+			public void mousePressed(MouseEvent e) {
+				mouseX = e.getX();
+				mouseY = e.getY();
+				mapAnimation.listCircle.add(new Circle( mouseX, mouseY));
+			}
+			public void mouseExited(MouseEvent e) {}
+			public void mouseEntered(MouseEvent e) {}
+			public void mouseClicked(MouseEvent e) {
+			}
+		};
+		MouseMotionListener mml = new MouseMotionListener() {
+			public void mouseMoved(MouseEvent e) {}
+			public void mouseDragged(MouseEvent e) {
+				mouseX = e.getX();
+				mouseY = e.getY();
+				seaMouse.start();
+			}
+		};
+		addMouseListener(ml);
+		addMouseMotionListener(mml);
 
 	}
 
