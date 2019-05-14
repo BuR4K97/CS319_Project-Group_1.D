1341bbe54adac9bcc62c1965664f69b98c3b749f
Nurlan Farzaliyev <47740270+Nurl4n@users.noreply.github.com>
2019-05-14 22:57:37 +0300

    ingame menu

--- src/Risk_Takers _Git/src/UIComponents/GamePanel.java
+++ src/Risk_Takers _Git/src/UIComponents/GamePanel.java
@@ -1,14 +1,23 @@
 package UIComponents;
 
 import java.awt.Color;
-
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
+
+import javax.swing.KeyStroke;
 
 import AnimationComponents.AnimationHandler;
 import Controller.GameController;
+import GameAssets.SoundConstants;
 import ModelClasses.Territory;
+import javax.swing.JLabel;
 
 public class GamePanel extends DynamicPanel {
 
@@ -20,12 +29,23 @@ public class GamePanel extends DynamicPanel {
 	private TextualInGamePanel textualInGamePanel;
 	private VisualCardPanel visualCardPanel;
 	private MouseInGameListener mouseTracer;
+	public boolean escPressed;
+	
+	private JLabel inGameMenuLabel;
+	private JLabel mainMenuLabel;
+	private JLabel backToGameLabel;
+	private JLabel quitLabel;
+	
+	//private JLabel quitInGame;
 	
 	public GamePanel() {
+		
 		if(GameController.activeMode == null) return;
+		this.setPreferredSize(new Dimension(1920, 1080));
 		setLayout(null);
 		setBackground(Color.BLACK);
-
+		setFocusable(true);
+		requestFocusInWindow();
 		
 		currState = PANEL_STATES.NORMAL;
 		mouseTracer = new MouseInGameListener();
@@ -38,10 +58,51 @@ public class GamePanel extends DynamicPanel {
 		textualInGamePanel.initialize(mouseTracer);
 		visualCardPanel = new VisualCardPanel();
 		visualCardPanel.initialize(mouseTracer);
+		
 
 		visualTerritoryPanel.insertMouseListeners(this);
 		textualInGamePanel.insertLabels(this);
 		interactionPanel.insertButtons(this);
+		
+		
+		inGameMenuLabel = new JLabel("");
+		inGameMenuLabel.setFont(new Font("Baskerville Old Face", Font.BOLD, 50));
+		inGameMenuLabel.setBounds(0, 0, 30, 30);
+		inGameMenuLabel.addMouseListener(new MouseListener() {
+			public void mouseReleased(MouseEvent e) {}
+			public void mousePressed(MouseEvent e) {}
+			public void mouseExited(MouseEvent e) {}
+			public void mouseEntered(MouseEvent e) {
+				SoundConstants.menuMouseOnButtonSound();
+			}
+			public void mouseClicked(MouseEvent e) {
+				if (escPressed)
+					escPressed = false;
+				else
+					escPressed = true;
+				SoundConstants.snapSound();
+			}
+		});	
+		add(inGameMenuLabel);
+		
+//		mainMenuLabel = new JLabel("MAIN MENU");
+//		mainMenuLabel.setFont(new Font("pixel", Font.BOLD, 50));
+//		mainMenuLabel.setForeground(Color.WHITE);
+//		mainMenuLabel.setBounds(800, 340, 300, 51);
+//		add(mainMenuLabel);
+//		
+//		backToGameLabel = new JLabel("BACK TO RISK");
+//		backToGameLabel.setFont(new Font("pixel", Font.BOLD, 50));
+//		backToGameLabel.setForeground(Color.WHITE);
+//		backToGameLabel.setBounds(780, 520, 400, 51);
+//		add(backToGameLabel);
+//		
+//		quitLabel = new JLabel("QUIT");
+//		quitLabel.setFont(new Font("pixel", Font.BOLD, 50));
+//		quitLabel.setForeground(Color.WHITE);
+//		quitLabel.setBounds(890, 700, 200, 51);
+//		add(quitLabel);
+		
 	}
 		
 	public void initializeAttackScenario(Territory[] combatTerritories) {
@@ -66,15 +127,27 @@ public class GamePanel extends DynamicPanel {
 		currState = PANEL_STATES.NORMAL;
 		interactionPanel.deactivateCardMode();
 	}
-
+	
+	int inGameSettingsPanelWidth = 800;
+	int inGameSettingsPanelHeight = 500;
 	public void paintComponent(Graphics painter) {
 		super.paintComponent(painter);
 		
 		visualTerritoryPanel.paint(painter);
 		if(!suspendVisualCardPanelPaintEvent())
 			visualCardPanel.paint(painter);
-		if(!suspendTextualInGamePanelPaintEvent()) {
+		if(!suspendTextualInGamePanelPaintEvent())
 			textualInGamePanel.paint(painter);
+		painter.setColor(Color.white);
+		painter.fillRect(0, 0, 30, 5);
+		painter.fillRect(0, 12, 30, 5);
+		painter.fillRect(0, 25, 30, 5);
+		
+		if(escPressed) {
+			painter.setColor(new Color(0, 0, 0, 220));
+			painter.fillRect(1920/2 - inGameSettingsPanelWidth / 2, 1080 / 2 - inGameSettingsPanelHeight / 2, inGameSettingsPanelWidth, inGameSettingsPanelHeight);
+			painter.setColor(Color.CYAN);
+			painter.drawRect(1920/2 - inGameSettingsPanelWidth / 2, 1080 / 2 - inGameSettingsPanelHeight / 2, inGameSettingsPanelWidth, inGameSettingsPanelHeight);
 		}
 	}
 
@@ -88,6 +161,7 @@ public class GamePanel extends DynamicPanel {
 		mouseTracer.mouseClicked = false;
 		mouseTracer.leftButtonClicked = false;
 		mouseTracer.rightButtonClicked = false;
+		
 	}
 	
 	public void requestNotification(String message) {
@@ -164,8 +238,4 @@ public class GamePanel extends DynamicPanel {
 			return true;
 		return AnimationHandler.suspendVisualCardPanelPaintEvent();
 	}
-
-
-
-	
 }
